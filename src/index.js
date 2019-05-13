34ec8c36478dd3477d15b89b7c654988df75757b
Payton Lower <paytown@me.com>
2019-05-13 12:03:42 -0700

    template for dog list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('banana');
+const root = document.getElementById('root');
+
+function makeDogs() {
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <ul>
+      <li>Spot</li>
+      <li>Rover</li>
+      <li>Bing</li>
+    </ul>
+  `;
+
+  return template.content;
+}
+
+function loadDogs() {
+  const dom = makeDogs();
+  root.appendChild(dom);
+}
+
+loadDogs();
